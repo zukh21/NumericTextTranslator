719c36c9b146f3ae1a2015d25d596493c805aa16
Zukhridin <zukh.kamchybekov@admins-MacBook-Pro.local>
2023-11-24 17:24:15 +0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,45 @@
+fun getNumberNameWithFinalChars(number: Int): String {
+    val units = arrayOf("нулевой", "первый", "второй", "третий", "четвертый", "пятый", "шестой", "седьмой", "восьмой", "девятый", "десятый")
+    val teens = arrayOf("одиннадцатый", "двенадцатый", "тринадцатый", "четырнадцатый", "пятнадцатый", "шестнадцатый", "семнадцатый", "восемнадцатый", "девятнадцатый")
+    val tens = arrayOf("", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто")
+    val tensZero = arrayOf("", "", "двадцатый", "тридцатый", "сороковой", "пятьдесятый", "шестьдесятый", "семьдесятый", "восемьдесятый", "девяностый")
+    val hundreds = arrayOf("", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот")
+    val hundredsZero = arrayOf("", "сотый", "двухсотый", "трехсотый", "четырехсотый", "пятисотый", "шестисотый", "семисотый", "восьмисотый", "девятисотый")
+
+    if (number < 0 || number > 999) {
+        return "Неверное число для преобразования"
+    }
+
+    return when {
+        number == 0 -> units[number]
+        number in 1..10 -> units[number]
+        number in 11..19 -> teens[number - 11]
+        number in 20..99 -> {
+            val digit1 = number / 10
+            val digit2 = number % 10
+            if (digit2 == 0) {
+                tensZero[digit1]
+            } else {
+                "${tens[digit1]} ${units[digit2]}"
+            }
+        }
+        number >= 100 -> {
+            val digit1 = number / 100
+            val digit2 = (number / 10) % 10
+            val digit3 = number % 10
+            println("digit1: $digit1")
+            println("digit2: $digit2")
+            println("digit3: $digit3")
+            if (digit2 == 0 && digit3 == 0){
+                hundredsZero[digit1]
+            }else if (digit2 == 0){
+                "${hundreds[digit1]} ${units[digit3]}"
+            }else if (digit3 == 0){
+                "${hundreds[digit1]} ${tensZero[digit2]}"
+            }else{
+                "${hundreds[digit1]} ${tens[digit2]} ${units[digit3]}"
+            }
+        }
+        else -> "Неверное число для преобразования"
+    }
+}
